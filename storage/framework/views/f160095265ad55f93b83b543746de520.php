0bc1abe0d05fb252855330d815c0b10fc360f450
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:24:55 +0700

    unzipgit

---
+++ storage/framework/views/f160095265ad55f93b83b543746de520.php
@@ -0,0 +1,3 @@
+<li class="breadcrumb-item"><a href="<?php echo e(route('backend.dashboard')); ?>"><i class="fa-solid fa-cubes"></i><span class="d-none d-sm-inline-block">&nbsp;<?php echo e(__('Dashboard')); ?></span></a></li>
+
+<?php echo $slot; ?><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/components/backend/breadcrumbs.blade.php ENDPATH**/ ?>
