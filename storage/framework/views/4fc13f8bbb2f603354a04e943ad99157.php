6907ef9f1bd4c4e2be24e6e7e458d76476dab398
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:23:22 +0700

    unzipgit

---
+++ storage/framework/views/4fc13f8bbb2f603354a04e943ad99157.php
@@ -0,0 +1,161 @@
+<?php $__env->startSection('title'); ?> <?php echo e(__($module_action)); ?> <?php echo e(__($module_title)); ?> <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('breadcrumbs'); ?>
+<?php if (isset($component)) { $__componentOriginal73d065ab05177f56255c078a05ab5686 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal73d065ab05177f56255c078a05ab5686 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.breadcrumbs','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.breadcrumbs'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+    <?php if (isset($component)) { $__componentOriginal5e5b40e973d9c7c18a583cb81079b74e = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.breadcrumb-item','data' => ['type' => 'active','icon' => ''.e($module_icon).'']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.breadcrumb-item'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['type' => 'active','icon' => ''.e($module_icon).'']); ?><?php echo e(__($module_title)); ?> <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e)): ?>
+<?php $attributes = $__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e; ?>
+<?php unset($__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal5e5b40e973d9c7c18a583cb81079b74e)): ?>
+<?php $component = $__componentOriginal5e5b40e973d9c7c18a583cb81079b74e; ?>
+<?php unset($__componentOriginal5e5b40e973d9c7c18a583cb81079b74e); ?>
+<?php endif; ?>
+ <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal73d065ab05177f56255c078a05ab5686)): ?>
+<?php $attributes = $__attributesOriginal73d065ab05177f56255c078a05ab5686; ?>
+<?php unset($__attributesOriginal73d065ab05177f56255c078a05ab5686); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal73d065ab05177f56255c078a05ab5686)): ?>
+<?php $component = $__componentOriginal73d065ab05177f56255c078a05ab5686; ?>
+<?php unset($__componentOriginal73d065ab05177f56255c078a05ab5686); ?>
+<?php endif; ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="card">
+    <div class="card-body">
+
+        <?php if (isset($component)) { $__componentOriginal57a22d33ea7984d606412297cfe33b67 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal57a22d33ea7984d606412297cfe33b67 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.section-header','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.section-header'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+            <i class="<?php echo e($module_icon); ?>"></i> <?php echo e(__($module_title)); ?> <small class="text-muted"><?php echo e(__($module_action)); ?></small>
+
+             <?php $__env->slot('toolbar', null, []); ?> 
+                <?php if (isset($component)) { $__componentOriginal03c0e80d38d2a15cf58878ae679803f0 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal03c0e80d38d2a15cf58878ae679803f0 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.buttons.return-back','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.buttons.return-back'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal03c0e80d38d2a15cf58878ae679803f0)): ?>
+<?php $attributes = $__attributesOriginal03c0e80d38d2a15cf58878ae679803f0; ?>
+<?php unset($__attributesOriginal03c0e80d38d2a15cf58878ae679803f0); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal03c0e80d38d2a15cf58878ae679803f0)): ?>
+<?php $component = $__componentOriginal03c0e80d38d2a15cf58878ae679803f0; ?>
+<?php unset($__componentOriginal03c0e80d38d2a15cf58878ae679803f0); ?>
+<?php endif; ?>
+             <?php $__env->endSlot(); ?>
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal57a22d33ea7984d606412297cfe33b67)): ?>
+<?php $attributes = $__attributesOriginal57a22d33ea7984d606412297cfe33b67; ?>
+<?php unset($__attributesOriginal57a22d33ea7984d606412297cfe33b67); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal57a22d33ea7984d606412297cfe33b67)): ?>
+<?php $component = $__componentOriginal57a22d33ea7984d606412297cfe33b67; ?>
+<?php unset($__componentOriginal57a22d33ea7984d606412297cfe33b67); ?>
+<?php endif; ?>
+
+        <div class="row mt-4">
+            <div class="col">
+                <form method="post" action="<?php echo e(route('backend.settings.store')); ?>" class="form-horizontal" role="form">
+                    <?php echo csrf_field(); ?>
+
+
+                    <?php if(count(config('setting_fields', [])) ): ?>
+
+                    <?php $__currentLoopData = config('setting_fields'); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $section => $fields): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                    <div class="card card-accent-primary mb-4">
+                        <div class="card-header">
+                            <i class="<?php echo e(Arr::get($fields, 'icon', 'glyphicon glyphicon-flash')); ?>"></i>
+                            &nbsp;<?php echo e($fields['title']); ?>
+
+                        </div>
+                        <div class="card-body">
+                            <p class="text-muted"><?php echo e($fields['desc']); ?></p>
+
+                            <div class="row mt-3">
+                                <div class="col">
+                                    <?php $__currentLoopData = $fields['elements']; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $field): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                                    <?php if ($__env->exists('backend.settings.fields.' . $field['type'] )) echo $__env->make('backend.settings.fields.' . $field['type'] , \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+                                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+
+                    <?php endif; ?>
+
+                    <div class="row m-b-md">
+                        <div class="col-md-12">
+                            <?php if (isset($component)) { $__componentOriginal01f50869d947699da362d3a7b41e6d66 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal01f50869d947699da362d3a7b41e6d66 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.buttons.save','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.buttons.save'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal01f50869d947699da362d3a7b41e6d66)): ?>
+<?php $attributes = $__attributesOriginal01f50869d947699da362d3a7b41e6d66; ?>
+<?php unset($__attributesOriginal01f50869d947699da362d3a7b41e6d66); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal01f50869d947699da362d3a7b41e6d66)): ?>
+<?php $component = $__componentOriginal01f50869d947699da362d3a7b41e6d66; ?>
+<?php unset($__componentOriginal01f50869d947699da362d3a7b41e6d66); ?>
+<?php endif; ?>
+                        </div>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="card-footer">
+        <div class="row">
+
+        </div>
+    </div>
+</div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('backend.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/backend/settings/index.blade.php ENDPATH**/ ?>
