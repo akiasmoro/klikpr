ab30acbdbbc00d72b78c149b6ca787e08802e092
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:22:34 +0700

    unzipgit

---
+++ storage/framework/views/0287181ea2c8bb00caa035f3e8ba6e31.php
@@ -0,0 +1,8 @@
+<div class="row">
+    <div class="col">
+        <small class="float-end text-muted">
+            <?php echo e($slot); ?>
+
+        </small>
+    </div>
+</div><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/components/backend/section-footer.blade.php ENDPATH**/ ?>
