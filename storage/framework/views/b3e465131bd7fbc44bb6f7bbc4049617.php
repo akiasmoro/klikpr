b1a70f1a5758150ca9b3af25b5bd51dfc76c48f8
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:24:13 +0700

    unzipgit

---
+++ storage/framework/views/b3e465131bd7fbc44bb6f7bbc4049617.php
@@ -0,0 +1,9 @@
+<?php $__env->startPush('after-styles'); ?>
+<!-- Lightbox2 CSS -->
+<link href="<?php echo e(asset('vendor/lightbox2/css/lightbox.min.css')); ?>" rel="stylesheet">
+<?php $__env->stopPush(); ?>
+
+<?php $__env->startPush('after-scripts'); ?>
+<!-- Lightbox2 JS -->
+<script src="<?php echo e(asset('vendor/lightbox2/js/lightbox.min.js')); ?>"></script>
+<?php $__env->stopPush(); ?><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/components/library/lightbox.blade.php ENDPATH**/ ?>
