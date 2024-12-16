66d7cc18f649ed9e588614ad7d0fe2c3f6837c19
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:10:06 +0700

    unzipgit

---
+++ Modules/Post/lang/id/posts.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'name' => 'Nama',
+    'slug' => 'Slug',
+    'created_by_alias' => 'Nama Penulis',
+    'intro' => 'Intro',
+    'content' => 'Konten',
+    'image' => 'Gambar',
+    'category_id' => 'Kategori',
+    'type' => 'Tipe',
+    'is_featured' => 'Jadikan Gambar Unggulan',
+    'tags' => 'Tags',
+    'status' => 'Status',
+    'published_at' => 'Dipublikasikan pada',
+
+    'meta_title' => 'Judul Meta',
+    'meta_keywords' => 'Kata kunci Meta',
+    'meta_description' => 'Deskripsi Meta',
+    'meta_og_image' => 'Open Graph Image',
+    'meta_og_url' => 'Open Graph URL',
+    'order' => 'Order',
+
+];
