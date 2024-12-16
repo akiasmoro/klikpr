42dc94e46c098760621aa39c4717ddfcef642e2d
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:06:31 +0700

    unzipgit

---
+++ app/Models/Pengaduan.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Presenters\UserPresenter;
+use App\Models\Traits\HasHashedMediaTrait;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Auth;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\Permission\Traits\HasRoles;
+
+
+class Pengaduan extends Authenticatable implements HasMedia, MustVerifyEmail
+{
+    use HasFactory;
+    use HasHashedMediaTrait;
+    use HasRoles;
+    use Notifiable;
+    use SoftDeletes;
+    use UserPresenter;
+
+    
+    protected $guarded = [
+        'updated_at',
+    ];
+    protected $table = 'pengaduan';
+
+
+    public static function increatmentId()
+    {
+        $id = 0;
+        $model = DB::table('pengaduan')->count();
+        if ($model) {
+            $id = $model + 1;
+        }
+        return $id;
+    }
+
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        // create a event to happen on creating
+        static::creating(function ($table) {
+            $table->id = Self::increatmentId();
+            $table->email = Auth::user()->email;
+        });
+    }
+}
+
