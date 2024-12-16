14455ba0c366cd51af39cd81082a06ecea600be6
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:24:27 +0700

    unzipgit

---
+++ storage/framework/views/cf97b0740090677a75ab8408bb312d63.php
@@ -0,0 +1,13 @@
+<?php if(setting('google_analytics') !== ""): ?>
+<?php
+$google_analytics = setting('google_analytics');
+?>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=<?php echo e($google_analytics); ?>"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '<?php echo e($google_analytics); ?>');
+</script>
+<?php endif; ?><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/components/google-analytics.blade.php ENDPATH**/ ?>
