db6045276d413200aeca22fa30556d6bf8a28583
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:24:07 +0700

    unzipgit

---
+++ storage/framework/views/ae0747349a6760db46732e027374646c.php
@@ -0,0 +1,170 @@
+<?php
+$module_icon = 'fa-solid fa-list-check';
+?>
+
+<?php $__env->startSection('title'); ?>
+    <?php echo e(__('Log Viewer Dashboard')); ?>
+
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('breadcrumbs'); ?>
+    <?php if (isset($component)) { $__componentOriginal73d065ab05177f56255c078a05ab5686 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal73d065ab05177f56255c078a05ab5686 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.breadcrumbs','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.breadcrumbs'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+        <?php if (isset($component)) { $__componentOriginal5e5b40e973d9c7c18a583cb81079b74e = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.breadcrumb-item','data' => ['type' => 'active','icon' => ''.e($module_icon).'']] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.breadcrumb-item'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['type' => 'active','icon' => ''.e($module_icon).'']); ?>
+            <?php echo app('translator')->get('Log Viewer'); ?>
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e)): ?>
+<?php $attributes = $__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e; ?>
+<?php unset($__attributesOriginal5e5b40e973d9c7c18a583cb81079b74e); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal5e5b40e973d9c7c18a583cb81079b74e)): ?>
+<?php $component = $__componentOriginal5e5b40e973d9c7c18a583cb81079b74e; ?>
+<?php unset($__componentOriginal5e5b40e973d9c7c18a583cb81079b74e); ?>
+<?php endif; ?>
+     <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal73d065ab05177f56255c078a05ab5686)): ?>
+<?php $attributes = $__attributesOriginal73d065ab05177f56255c078a05ab5686; ?>
+<?php unset($__attributesOriginal73d065ab05177f56255c078a05ab5686); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal73d065ab05177f56255c078a05ab5686)): ?>
+<?php $component = $__componentOriginal73d065ab05177f56255c078a05ab5686; ?>
+<?php unset($__componentOriginal73d065ab05177f56255c078a05ab5686); ?>
+<?php endif; ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+    <div class="card mb-4">
+        <div class="card-body">
+            <?php if (isset($component)) { $__componentOriginal57a22d33ea7984d606412297cfe33b67 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal57a22d33ea7984d606412297cfe33b67 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.section-header','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.section-header'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+                <?php echo app('translator')->get('Log Dashboard'); ?>
+
+                 <?php $__env->slot('toolbar', null, []); ?> 
+                    <?php if (isset($component)) { $__componentOriginal03c0e80d38d2a15cf58878ae679803f0 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal03c0e80d38d2a15cf58878ae679803f0 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.buttons.return-back','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.buttons.return-back'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal03c0e80d38d2a15cf58878ae679803f0)): ?>
+<?php $attributes = $__attributesOriginal03c0e80d38d2a15cf58878ae679803f0; ?>
+<?php unset($__attributesOriginal03c0e80d38d2a15cf58878ae679803f0); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal03c0e80d38d2a15cf58878ae679803f0)): ?>
+<?php $component = $__componentOriginal03c0e80d38d2a15cf58878ae679803f0; ?>
+<?php unset($__componentOriginal03c0e80d38d2a15cf58878ae679803f0); ?>
+<?php endif; ?>
+                    <a class="btn btn-primary ms-1" type="button" href="<?php echo e(route('log-viewer::logs.list')); ?>">
+                        <i class="fas fa-list-ol"></i> <?php echo app('translator')->get('Daily Log'); ?>
+                    </a>
+                 <?php $__env->endSlot(); ?>
+             <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal57a22d33ea7984d606412297cfe33b67)): ?>
+<?php $attributes = $__attributesOriginal57a22d33ea7984d606412297cfe33b67; ?>
+<?php unset($__attributesOriginal57a22d33ea7984d606412297cfe33b67); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal57a22d33ea7984d606412297cfe33b67)): ?>
+<?php $component = $__componentOriginal57a22d33ea7984d606412297cfe33b67; ?>
+<?php unset($__componentOriginal57a22d33ea7984d606412297cfe33b67); ?>
+<?php endif; ?>
+
+            <div class="row">
+                <div class="col-md-6 col-lg-3">
+                    <canvas class="mb-3" id="stats-doughnut-chart" height="300"></canvas>
+                </div>
+
+                <div class="col-md-6 col-lg-9">
+                    <div class="row">
+                        <?php $__currentLoopData = $percents; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $level => $item): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                            <div class="col-sm-6 col-md-12 col-lg-4 mb-3">
+                                <div class="box level-<?php echo e($level); ?> <?php echo e($item['count'] === 0 ? 'empty' : ''); ?>">
+                                    <div class="box-icon">
+                                        <?php echo log_styler()->icon($level); ?>
+
+                                    </div>
+
+                                    <div class="box-content">
+                                        <span class="box-text"><?php echo e($item['name']); ?></span>
+                                        <span class="box-number">
+                                            <?php echo e($item['count']); ?> <?php echo app('translator')->get('entries'); ?> - <?php echo $item['percent']; ?> %
+                                        </span>
+                                        <div class="progress" style="height: 3px;">
+                                            <div class="progress-bar" style="width: <?php echo e($item['percent']); ?>%"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startPush('after-styles'); ?>
+    <?php echo $__env->make('log-viewer::laravel-starter.style', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopPush(); ?>
+
+<?php $__env->startPush('after-scripts'); ?>
+    <script type="module" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js"
+        integrity="sha512-SIMGYRUjwY8+gKg7nn9EItdD8LCADSDfJNutF9TPrvEo86sQmFMh6MyralfIyhADlajSxqc7G0gs7+MwWF/ogQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script type="module">
+        function ready(fn) {
+            if (document.readyState !== 'loading') {
+                fn();
+            } else {
+                document.addEventListener('DOMContentLoaded', fn);
+            }
+        }
+
+        ready(function() {
+            new Chart(document.getElementById("stats-doughnut-chart"), {
+                type: 'doughnut',
+                data: <?php echo $chartData; ?>,
+                options: {
+                    legend: {
+                        position: 'bottom'
+                    }
+                }
+            });
+        });
+    </script>
+<?php $__env->stopPush(); ?>
+
+<?php echo $__env->make('backend.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/vendor/log-viewer/laravel-starter/dashboard.blade.php ENDPATH**/ ?>
