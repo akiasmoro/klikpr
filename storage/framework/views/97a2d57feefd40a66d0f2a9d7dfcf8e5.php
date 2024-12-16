3e838115cebf8c903a7ad64676a477b5ddffd3ca
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:23:52 +0700

    unzipgit

---
+++ storage/framework/views/97a2d57feefd40a66d0f2a9d7dfcf8e5.php
@@ -0,0 +1,9 @@
+<table class="subcopy" width="100%" cellpadding="0" cellspacing="0" role="presentation">
+<tr>
+<td>
+<?php echo e(Illuminate\Mail\Markdown::parse($slot)); ?>
+
+</td>
+</tr>
+</table>
+<?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/vendor/mail/html/subcopy.blade.php ENDPATH**/ ?>
