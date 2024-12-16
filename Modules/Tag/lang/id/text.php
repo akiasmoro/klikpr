a6fe23b0a5fe45935e49885ec2d20735d8f61b12
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:10:41 +0700

    unzipgit

---
+++ Modules/Tag/lang/id/text.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'name' => 'Nama',
+    'slug' => 'Slug',
+    'description' => 'Deskripsi',
+    'status' => 'Status',
+    'created_by' => 'Diposting oleh',
+    'updated_at' => 'Update pada',
+    'updated_by' => 'Diupdate oleh',
+    'deleted_by' => 'Dihapus oleh',
+    'action' => 'Aksi',
+
+];
