a15d7c9c20e20e862e1151069819461e89ebe551
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:07:54 +0700

    unzipgit

---
+++ lang/id/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Username atau password tidak terdaftar.',
+    'password' => 'Password salah.',
+    'throttle' => 'Terlalu banyak percobaan logi. Silahkan coba lagi kembali selama :seconds seconds.',
+
+];
