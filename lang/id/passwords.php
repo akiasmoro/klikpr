0464cc6c9539f17539fb53ff8ecd5cb7536576dd
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:07:57 +0700

    unzipgit

---
+++ lang/id/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Pasword anda berhasil direset!',
+    'sent' => 'Kami telah mengirimkan link reset ke email anda!',
+    'throttled' => 'Silahkan tunggu dan coba lagi.',
+    'token' => 'Token password salah.',
+    'user' => "Kami tidak menemukan user tersebut.",
+
+];
