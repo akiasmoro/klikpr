d46ed84766bddcf26e7bef3d38b6d5e1c58d46df
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:22:55 +0700

    unzipgit

---
+++ storage/framework/views/1ae0fb7993fc093f243548be50fd9f27.php
@@ -0,0 +1,69 @@
+<?php $__env->startSection('title'); ?> <?php echo app('translator')->get("Dashboard"); ?> <?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('breadcrumbs'); ?>
+<?php if (isset($component)) { $__componentOriginal73d065ab05177f56255c078a05ab5686 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal73d065ab05177f56255c078a05ab5686 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.breadcrumbs','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.breadcrumbs'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal73d065ab05177f56255c078a05ab5686)): ?>
+<?php $attributes = $__attributesOriginal73d065ab05177f56255c078a05ab5686; ?>
+<?php unset($__attributesOriginal73d065ab05177f56255c078a05ab5686); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal73d065ab05177f56255c078a05ab5686)): ?>
+<?php $component = $__componentOriginal73d065ab05177f56255c078a05ab5686; ?>
+<?php unset($__componentOriginal73d065ab05177f56255c078a05ab5686); ?>
+<?php endif; ?>
+<?php $__env->stopSection(); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="card mb-4 ">
+    <div class="card-body">
+
+        <?php if (isset($component)) { $__componentOriginal57a22d33ea7984d606412297cfe33b67 = $component; } ?>
+<?php if (isset($attributes)) { $__attributesOriginal57a22d33ea7984d606412297cfe33b67 = $attributes; } ?>
+<?php $component = Illuminate\View\AnonymousComponent::resolve(['view' => 'components.backend.section-header','data' => []] + (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag ? $attributes->all() : [])); ?>
+<?php $component->withName('backend.section-header'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\Illuminate\View\AnonymousComponent::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes([]); ?>
+            <?php echo app('translator')->get("Admin Dashboard"); ?>
+
+             <?php $__env->slot('toolbar', null, []); ?> 
+                <button class="btn btn-outline-primary mb-1" type="button" data-toggle="tooltip" data-coreui-placement="top" title="Tooltip">
+                    <i class="fa-solid fa-bullhorn"></i>
+                </button>
+             <?php $__env->endSlot(); ?>
+         <?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__attributesOriginal57a22d33ea7984d606412297cfe33b67)): ?>
+<?php $attributes = $__attributesOriginal57a22d33ea7984d606412297cfe33b67; ?>
+<?php unset($__attributesOriginal57a22d33ea7984d606412297cfe33b67); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginal57a22d33ea7984d606412297cfe33b67)): ?>
+<?php $component = $__componentOriginal57a22d33ea7984d606412297cfe33b67; ?>
+<?php unset($__componentOriginal57a22d33ea7984d606412297cfe33b67); ?>
+<?php endif; ?>
+
+        <!-- Dashboard Content Area -->
+        
+        <!-- / Dashboard Content Area -->
+
+    </div>
+</div>
+
+
+<?php echo $__env->make("backend.includes.dashboard", \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('backend.layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/backend/index.blade.php ENDPATH**/ ?>
