8094c48ae31214f0d26d66f564e5a6bfe33920f6
A Ki Asmoro Santo <60425220+akiasmoro@users.noreply.github.com>
2024-12-16 15:23:43 +0700

    unzipgit

---
+++ storage/framework/views/7c52603f4b6a826f73abe5cc35dca34a.php
@@ -0,0 +1,3 @@
+<div class="py-6">
+    <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400"><?php echo setting('footer_text'); ?></span>
+</div><?php /**PATH Y:\Project\2024\klikpr2\ori\laravel-starter-main\resources\views/components/frontend/footer-credit.blade.php ENDPATH**/ ?>
