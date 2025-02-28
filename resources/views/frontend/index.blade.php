f6eaa547e20a19fd45b60e52934eb41cd1cd427e
GeryMs19 <94509046+GeryMs19@users.noreply.github.com>
2025-02-28 21:34:20 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -129,10 +129,10 @@
                                     class="text-3xl md:text-6xl font-bold animate__animated animate__fadeIn text-black/80 mb-6 text-nowrap ">
                                     Selamat datang di Portal</h1>
                                 <h2
-                                    class="text-2xl md:text-3xl font-extrabold animate__animated animate__fadeIn text-yellow-500 mb-3 md:mb-3">
+                                    class="text-2xl md:text-3xl font-extrabold animate__animated animate__fadeIn text-yellow-400 mb-3 md:mb-3">
                                     TATA RUANG</h2>
                                 <h3
-                                    class="text-xl md:text-3xl font-extrabold animate__animated animate__fadeIn text-yellow-500 mb-6 px-0 md:max-w-[900px] ">
+                                    class="text-md md:text-3xl font-extrabold animate__animated animate__fadeIn text-yellow-400 mb-6 px-0 md:max-w-[900px] ">
                                     DINAS PERUMAHAN DAN PERMUKIMAN <br> KOTA BANDAR LAMPUNG</h3>
                                 <p
                                     class="text-md text-black/70 bg-white/45 p-2 rounded-md font-bold mb-8  max-w-2xl mx-auto">
